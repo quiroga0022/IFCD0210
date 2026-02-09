5d97c25a076ae39df45ef93e42c7e2d362120f6b
quiroga0022 <jorgequiroga89@gmail.com>
2026-02-09 17:26:21 +0100

    nueve febrero ok

--- UF1844/flask/02_microbit/app.py
+++ UF1844/flask/02_microbit/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def hola():
     return render_template('home.html')
+
+@app.route('/software')
+def get_software():
+    return render_template('software.html')
+
+@app.route('/firmware')
+def get_firmware():
+    return render_template('firmware.html')
